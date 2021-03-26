6e535b2643582e01652336f6a01f9e0ecae1d866
leonpaulo723 <psleon1@misena.edu.co>
2021-03-26 00:49:24 -0500

    comit2_templates

--- home/urls.py
+++ home/urls.py
@@ -6,9 +6,14 @@ from .views import *
 urlpatterns = [
 
 
-path('about/',vista_about),
-
-path('contact/',vista_contact),
+path('equipo/',vista_equipo),
+path('inicio/',vista_inicio),
+path('servicio/',vista_servicio),
+path('productos/',vista_productos),
+path('contacto/',vista_contacto),
+path('registro/',vista_registro),
+path('inicio_sesion/',vista_inicio_sesion),
+path('solicitar_cita/',vista_solicitar_cita),
 
 
 
--- home/views.py
+++ home/views.py
@@ -2,8 +2,33 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def vista_about(request):
-    return render(request, 'about.html')
+def vista_equipo(request):
+    return render(request, 'equipo.html')
 
-def vista_contact(request):
-    return render(request,'contact.html')
+def vista_inicio(request):
+    return render(request,'inicio.html')
+
+def vista_servicio(request):
+    return render(request,'servicio.html')
+
+def vista_productos(request):
+    return render(request,'productos.html')
+
+def vista_contacto(request):
+    return render(request,'contacto.html')
+
+def vista_registro(request):
+    return render(request,'registro.html')
+
+def vista_inicio_sesion(request):
+    return render(request,'inicio_sesion.html')
+
+def vista_solicitar_cita(request):
+    return render(request,'solicitar_cita.html')
+
+        
+    
+
+
+
+    
